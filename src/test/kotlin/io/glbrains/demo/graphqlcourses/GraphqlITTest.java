dbe26ff19b39360cc276a072566df3feca210ccc
Alex SIEYADJEU <alex.sieyadjeu@lyaprotect.com>
2023-07-12 11:04:12 +0200

    unit test

--- src/test/kotlin/io/glbrains/demo/graphqlcourses/GraphqlITTest.java
+++ src/test/kotlin/io/glbrains/demo/graphqlcourses/GraphqlITTest.java
@@ -1,8 +1,10 @@
 package io.glbrains.demo.graphqlcourses;
 
 import com.graphql.spring.boot.test.GraphQLTest;
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
 import io.gqlbrains.demo.graphqlcourses.GraphqlCoursesApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.context.annotation.Import;
 
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Import;
 @AutoConfigureDataJpa
 public class GraphqlITTest {
 
+    @Autowired
+    private GraphQLTestTemplate graphQLTestTemplate;
     @Test
     void loadContext() {
 
